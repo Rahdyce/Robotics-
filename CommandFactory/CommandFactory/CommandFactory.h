caa5dc072f19480c3e84e8caf4bc85bcd4e3026a
pjaxon48 <59519727+pjaxon48@users.noreply.github.com>
2024-02-26 18:01:34 -0500

    Create CommandFactory.h

---
+++ CommandFactory/CommandFactory/CommandFactory.h
@@ -0,0 +1,32 @@
+/*
+  CommandFactory.h - Library for handling data transmission packets
+  between Pico and Pi.
+*/
+#ifndef CommandFactory_h
+#define CommandFactory_h
+
+#include "Arduino.h"
+
+class CommandFactory
+{
+	public:
+		CommandFactory(void);
+		String command(void);
+		void interpret(String command);
+		void writer(String argument, int value);
+		void writer(int sel, int value);
+		int reader(String argument);
+		int reader(int sel);
+	private:
+		int LMSpeed;
+		int RMSpeed;
+		int baseAngle;
+		int shoulderAngle;
+		int elbowAngle;
+		int wristAngle;
+		int swivelAngle;
+		int clawAngle;
+		int trayDoor;
+};
+
+#endif
