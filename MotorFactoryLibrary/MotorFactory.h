1c4ee8488492c4b5c59ac1dce5c3066a33a497a9
Rahdyce <147444223+Rahdyce@users.noreply.github.com>
2024-02-26 19:25:47 -0500

    Update MotorFactory.h

--- MotorFactoryLibrary/MotorFactory.h
+++ MotorFactoryLibrary/MotorFactory.h
@@ -12,8 +12,8 @@ class MotorFactory
 	
 	public:
 		MotorFactory(void);
-		void servoPins(int base, int shoulder, int elbow, int wrist, int swivel, int claw, int tray);
-		void motorPins(int leftmotor, int rightmotor, int leftspeed, int rightspeed);
+		void setservoPins(int base, int shoulder, int elbow, int wrist, int swivel, int claw, int tray);
+		void setmotorPins(int leftmotor, int rightmotor, int leftspeed, int rightspeed);
 		void setServo(String servoname, int angle);
 		void setDrive(String name, int speed);
 	private:
