c321e76a9ee12375797ce8fa69c4f11b379923cc
Jeremy Randolph <85804045+JR16CS@users.noreply.github.com>
2024-03-14 14:15:24 -0400

    Update sensorFactory.ino

--- sensorFactory.ino
+++ sensorFactory.ino
@@ -1,7 +1,12 @@
+#include <Wire.h>
+#include <Adafruit_TCS34725.h>
 class sensorFactory{
-  private: int irPin, triggerPin, echoPin, sensorNumber;
-  public: 
-  void setIRPdein(int irPin, int sensorNumber){
+    Adafruit_TCS34725 tcs = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_50MS, TCS34725_GAIN_4X);
+    
+  private:
+    int irPin, triggerPin, echoPin, sensorNumber;
+  public:
+  void setIRPin(int irPin, int sensorNumber){
     this -> irPin = irPin;
     this -> sensorNumber = sensorNumber;
     pinMode(irPin, INPUT);
@@ -13,19 +18,35 @@ class sensorFactory{
     pinMode(triggerPin, OUTPUT);
     pinMode(echoPin, INPUT);
   }
+  void setColorPin(int sdaPin, int sclPin){
+      Wire.setSDA(sdaPin);
+      Wire.setSCL(sclPin);
+      Wire.begin();
+  }
   bool getLeftIR(int irPin){
-    return analogRead(irPin);} 
+    return digitalRead(irPin);} 
+  
   bool getRightIR(int irPin){
-    return analogRead(irPin);} 
+    return digitalRead(irPin);}
+  
   bool getClawIR(int irPin){
-    return analogRead(irPin);} 
-  bool color(int lowColor, int highColor, int irPin){
-    int sensorValue = irPin;
-    if(lowColor<=irPin && irPin<=highColor){
+    return digitalRead(irPin);}
+  
+  bool greenLight(){
+    uint16_t r, g, b, c, colorTemp, lux;
+    tcs.getRawData(&r, &g, &b, &c);
+    colorTemp = tcs.calculateColorTemperature(r, g, b);
+    lux = tcs.calculateLux(r, g, b);
+    Serial.print("Color Temp: "); Serial.print(colorTemp, DEC); Serial.print(" K - "); 
+    Serial.print("Lux: "); Serial.print(lux, DEC); Serial.print(" - "); 
+    Serial.print("R: "); Serial.print(r, DEC); Serial.print(" ");
+    Serial.print("G: "); Serial.print(g, DEC); Serial.print(" ");
+    Serial.print("B: "); Serial.print(b, DEC); Serial.print(" ");
+    Serial.print("C: "); Serial.print(c, DEC); Serial.println(" ");
+    if(g>(r*1.5||b*1.5)){
       return true;}
     else{
-      return false;
-    }
+      return false;}
   }
   int sonicDistanceSensor(int echoPin,int triggerPin){
     int distance,duration,cm,inch,count=0;
