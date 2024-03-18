3ea8b0ce8022a33bbfd1874a207e5b4a910ecdfc
David Minott <67799380+sepasfr@users.noreply.github.com>
2024-03-18 13:48:04 -0400

    Update INTERPOLATER.py
    Returns only integer values, no more decimals

--- PRODUCTION/DAQAT/INTERPOLATER.py
+++ PRODUCTION/DAQAT/INTERPOLATER.py
@@ -5,25 +5,35 @@ def generate_intermediate_waypoints():
     num_waypoints = int(input("How many waypoints in between?: "))
 
     # extract the values of each part of the arm in the waypoint
-    start_vals = [int(x.split('=')[1]) for x in start.split('-')[1:-1]]
-    end_vals = [int(x.split('=')[1]) for x in end.split('-')[1:-1]]
+    start_parts = start.split('-')
+    end_parts = end.split('-')
     
-    # calculate the difference between start and end values
-    #in case you arent sure because people do not use it often, zip takes iterable containers and returns a single iterator object. 
-    diff_vals = [end_val - start_val for start_val, end_val in zip(start_vals, end_vals)]
+    # Extract X value as integer for processing
+    start_x_value = int([part for part in start_parts if part.startswith("X=")][0].split('=')[1])
+    end_x_value = int([part for part in end_parts if part.startswith("X=")][0].split('=')[1])
+
+    # Determine X value for intermediate waypoints based on the rule provided
+    intermediate_x_value = 1 if end_x_value == 1 else start_x_value
+
+    # exclude 'A', 'X=value', and 'Z' to only get the numeric parts
+    start_vals = [int(x.split('=')[1]) for x in start_parts if "=" in x and not x.startswith("X=")]
+    end_vals = [int(x.split('=')[1]) for x in end_parts if "=" in x and not x.startswith("X=")]
+
+    # calculate the difference between start and end values including X
+    diff_vals = [end_val - start_val for start_val, end_val in zip(start_vals + [start_x_value], end_vals + [end_x_value])]
     
     # calculate the step for each value for the intermediate waypoints so that they are spaced out evenly
     steps = [diff / (num_waypoints + 1) for diff in diff_vals]
     
-    # now generate the set of intermediate waypoints
+    # generate the set of intermediate waypoints
     waypoints = []
     for i in range(1, num_waypoints + 1):
-        intermediate_vals = [round(start_val + step * i, 2) for start_val, step in zip(start_vals, steps)]
-        waypoint = f"A-L={intermediate_vals[0]}-R={intermediate_vals[1]}-B={intermediate_vals[2]}-S={intermediate_vals[3]}-E={intermediate_vals[4]}-R={intermediate_vals[5]}-W={intermediate_vals[6]}-C={intermediate_vals[7]}-TD={intermediate_vals[8]}-Z"
+        intermediate_vals = [round(start_val + step * i) for start_val, step in zip(start_vals + [start_x_value], steps)]
+        # format
+        waypoint_parts = [f"L={intermediate_vals[0]}", f"R={intermediate_vals[1]}", f"B={intermediate_vals[2]}",
+                          f"S={intermediate_vals[3]}", f"E={intermediate_vals[4]}", f"R={intermediate_vals[5]}",
+                          f"W={intermediate_vals[6]}", f"C={intermediate_vals[7]}", f"TD={intermediate_vals[8]}", f"X={intermediate_x_value}"]
+        waypoint = "A-" + "-".join(waypoint_parts) + "-Z"
         waypoints.append(waypoint)
     
     return waypoints
-
-#test it
-print(generate_intermediate_waypoints())
-
