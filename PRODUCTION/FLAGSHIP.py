04a96916774c6a8e6aedef6f59e6d7e6ae8de499
David Minott <67799380+sepasfr@users.noreply.github.com>
2024-03-20 16:34:35 -0400

    Update FLAGSHIP.py
    added string interpreter for sensor data. Will look at -IL= and -IR= for the current sensor data states

--- PRODUCTION/FLAGSHIP.py
+++ PRODUCTION/FLAGSHIP.py
@@ -1,6 +1,7 @@
 import serial
 import os
-from SensorFactory import # The name of the method that will send the sensor_packet
+import sys
+import time
 
 #Global Variables for ease of modification
 COM_PORT = "COM3"
@@ -46,20 +47,20 @@ def set_arm(command_packet):
 #function that gets us over the ramp (steps 6-8)
 def ramp_run(ir_sensor_1, ir_sensor_2, tof_sensor):
     #If both IR Sensors are on Black, both will return true meaning we are aligned (atleast somewhat)
-    if ir_sensor_1 == True and ir_sensor_2 == True:
+    if ir_sensor_1 == "-IL=1" and ir_sensor_2 == "-IR=1":
         #playback the run up the ramp
         playback("CLIMB.txt")
     #in case we get stuck on ramp, we can have an emergency code to get us up. 5mm is a placeholder for now
     if tof_sensor > 5 and tof_sensor < TOF_THRESHOLD:
         playback("EM_RAMP_CLIMB.txt")
         #If we are pointing left, this means ir_sensor_2 will be false and ir_sensor_1 will be true (ir_sensor_2 is the rightmost sensor)
-    if tof_sensor > TOF_THRESHOLD and ir_sensor_1 == True and ir_sensor_2 == False:
+    if tof_sensor > TOF_THRESHOLD and ir_sensor_1 == "-IL=1" and ir_sensor_2 == "-IR=0":
         playback("ADJUST_RIGHT.txt")
     #now do the opposite if it is pointing right
-    if tof_sensor > TOF_THRESHOLD and ir_sensor_1 == False and ir_sensor_2 == True:
+    if tof_sensor > TOF_THRESHOLD and ir_sensor_1 == "-IL=0" and ir_sensor_2 == "-IR=1":
         playback("ADJUST_LEFT.txt")
     #If our time of flight sensor returns the value we want (greater than 610mm or 2feet) we know we are at the top of the ramp, and if we are still aligned with the line this means we are good to go and descend down the ramp
-    if tof_sensor > TOF_THRESHOLD and ir_sensor_1 == True and ir_sensor_2 == True:
+    if tof_sensor > TOF_THRESHOLD and ir_sensor_1 == "-IL=1" and ir_sensor_2 == "-IR=1":
         playback("DESCENT_POSITION.txt")
         # Start descent
         while True:
@@ -81,12 +82,12 @@ def robot_turn_and_wall(front_sensor):
       if front_sensor > 110:
             # If the robot angled too far to the left, adjust it to the right
             playback("ADJUST_RIGHT.txt")
-        elif 100 < front_sensor <= 110:
+      elif 100 < front_sensor <= 110:
             # If within a reasonable tolerance, initiate the turn left
             playback("TURN_LEFT.txt")
             break  # Exit the loop once the turn command is issued, then we can run the command to go forward
         #chances are the robot is angled to far to the right and is looking at the wall behind the boosters, adjust the robot left
-        elif front_sensor < 100:
+      elif front_sensor < 100:
             playback("ADJUST_LEFT.txt")
 
     #send the robot to the wall until the front sensor is at a distance where we can pick up the carrots, at t
@@ -105,7 +106,7 @@ def carrot_pickup():
 #function to get us across the gap
 def gap_cross(ir_sensor_1, ir_sensor_2, tof_threshold):
     #We are aligned but not at the top of the ramp
-    while ir_sensor_1 == 1 and ir_sensor_2 == 1 and tof_threshold < TOF_THRESHOLD:
+    while ir_sensor_1 == "A-IL=1" and ir_sensor_2 == "I-IR=1" and tof_threshold < TOF_THRESHOLD:
         playback("WALL_TO_GAP.txt")
         break
     #Adjust arm position and cross the gap (should all be done in GAP_CROSS.txt)
@@ -115,9 +116,9 @@ def gap_cross(ir_sensor_1, ir_sensor_2, tof_threshold):
 def depot(ir_sensor_1, ir_sensor_2):
     #If both IR Sensors are on Black, both will return true meaning we are aligned (atleast somewhat)
     while True:
-        if ir_sensor_1 == False and ir_sensor_2 == True:
+        if ir_sensor_1 == "-IL=0" and ir_sensor_2 == "-IR=1":
             playback("ADJUST_LEFT.txt")
-        elif ir_sensor_1 == True and ir_sensor_2 == False:
+        elif ir_sensor_1 == "-IL=1" and ir_sensor_2 == "-IR=0":
             playback("ADJUST_RIGHT.txt")
         else:
             break
@@ -129,6 +130,27 @@ def button_and_reset():
 
     #restart jetson
     os.system("sudo reboot")
+
+def interpret_sensor_data(sensor_string):
+    # Initialize default values for sensor status
+    ir_sensor_1_status = "0"
+    ir_sensor_2_status = "0"
+
+    # Find the positions of the IR sensor indicators in the string
+    ir_sensor_1_pos = sensor_string.find("-IL=")
+    ir_sensor_2_pos = sensor_string.find("-IR=")
+
+    # Extract the status if the indicators are found
+    if ir_sensor_1_pos != -1:
+        ir_sensor_1_status = sensor_string[ir_sensor_1_pos + 4]
+
+    if ir_sensor_2_pos != -1:
+        ir_sensor_2_status = sensor_string[ir_sensor_2_pos + 4]
+
+    ir_sensor_1 = f"-IL={ir_sensor_1_status}"
+    ir_sensor_2 = f"-IR={ir_sensor_2_status}"
+
+    return ir_sensor_1, ir_sensor_2
 
 def main():
     # Example sensor values to start with
